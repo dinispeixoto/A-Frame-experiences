9cc2e0503d52ed3c5b817b743df016ac92058532
Dinis <dinispeixoto96@gmail.com>
2018-03-02 16:11:19 +0000

    ok, they already look falling

--- Spaceship/meteors.js
+++ Spaceship/meteors.js
@@ -1,13 +1,14 @@
 // Meteors
-var number_of_meteors = 100;
-var meteor_height = 0.1;
-var meteor_width = 0.1;
-var meteor_depth = 0.1;
-var falling_distance = 10;
+var number_of_meteors = 500;
+var meteor_height = 0.2;
+var meteor_width = 0.2;
+var meteor_depth = 0.2;
+var min_falling_distance = 10;
+var max_falling_distance = 50;
 
 // Floor
-var floor_width = 40;
-var floor_height = 40;
+var floor_width = 25;
+var floor_height = 25;
 
 if (typeof AFRAME === 'undefined') {
   throw new Error('Component attempted to register before' +
@@ -40,12 +41,13 @@ function initFloor(){
 function generateMeteor(){
     x = Math.floor((Math.random() * floor_width) - floor_width/2);
     y = Math.floor((Math.random() * floor_height) - floor_height/2);
+    h = Math.floor((Math.random() * max_falling_distance - min_falling_distance)) + min_falling_distance;
+    v = Math.random()
 
-    var position = '' + x + ' ' + falling_distance + ' ' + y;
+    var position = '' + x + ' ' + h + ' ' + y;
 
-    var meteor = document.createElement('a-entity');
+    var meteor = document.createElement('a-box');
     meteor.setAttribute('geometry', {
-        primitive: 'box',
         height: meteor_height,
         width: meteor_width,
         depth: meteor_depth
@@ -54,8 +56,8 @@ function generateMeteor(){
     meteor.setAttribute('position', position);
     meteor.setAttribute('dynamic-body', {
         shape: 'box',
-        mass: 1.5,
-        linearDamping: 0.005
+        mass: 1,
+        linearDamping: 1
     });
 
     return meteor;
